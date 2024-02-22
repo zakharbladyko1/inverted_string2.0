8a995df22f58918bdba51a4c46fdaaae8774ea7a
Myself <Myself@DESKTOP-HUSV4M8>
2024-02-22 13:04:38 +0300

    Добавьте файлы проекта.

---
+++ main/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX = 100;
+
+int main()
+{
+    void reversit(char[]);
+    char a1[MAX];
+
+    cout << "Enter string : ";
+    cin.get(a1, MAX);
+
+    reversit(a1);
+
+    cout << "Now string : " << a1 << endl;
+
+    return 0;
+}
+
+void reversit(char rev[])
+{
+    int len = strlen(rev);
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = rev[i];
+        rev[i] = rev[len - i - 1];
+        rev[len - i - 1] = temp;
+    }
+}
